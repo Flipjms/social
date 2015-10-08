976a0f36b74a72128964da5ea1bd83b5f15fe0f9
Filipe Santos <flipjms@gmail.com>
2015-10-08 15:54:47 +0100

    Facebook posts can have no message field, so the migration was updated to allow nullable content

--- src/migrations/facebook/2015_08_18_161235_create_social_facebook_page_posts_table.php
+++ src/migrations/facebook/2015_08_18_161235_create_social_facebook_page_posts_table.php
@@ -3,42 +3,42 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSocialFacebookPagePostsTable extends Migration {
+class CreateSocialFacebookPagePostsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('facebook_posts',function(Blueprint $table){
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facebook_posts', function (Blueprint $table) {
 
-			$table->increments('id');
-			$table->string('external_id');
-			$table->boolean('active')->default(1);
-			$table->boolean('is_published');
+            $table->increments('id');
+            $table->string('external_id');
+            $table->boolean('active')->default(1);
+            $table->boolean('is_published');
 
-			$table->text('content');
-			$table->string('link')->nullable()->default(null);
-			$table->integer('likes')->nullable()->default(null);
-			$table->string('status_type');
-			$table->string('type');
+            $table->text('content')->nullable()->default(null);
+            $table->string('link')->nullable()->default(null);
+            $table->integer('likes')->nullable()->default(null);
+            $table->string('status_type');
+            $table->string('type');
 
-			$table->timestamp('published_at');
+            $table->timestamp('published_at');
 
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('facebook_posts');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('facebook_posts');
+    }
 }
